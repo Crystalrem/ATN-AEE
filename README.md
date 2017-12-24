dfc28e3096f32c4137fa8b56a3434d508745c7c5
SJTU-ACM <1031390651@qq.com>
2017-12-24 13:28:52 +0800

    New

---
+++ README.md
@@ -0,0 +1,36 @@
+# Adversarial Transformation Network(ATN)
+
+## Introduction
+
+  A simple implement of an Adversarial Autoencoding ATN(AAE ATN) proposed in [Adversarial Transformation Networks: Learning to Generate Adversarial Examples](https://arxiv.org/abs/1703.09387) using tensorflow.
+
+## Requirements
+
+python 3.5
+
+tensorflow 1.1.0
+
+matplotlib (for result visualizing)
+
+## Usage
+
+  You can test with my trained model:
+
+  ```shell
+  python atn.py
+  ```
+
+  If you want to train by yourself:
+  ```shell
+  python atn.py --train
+  ```
+
+## Result
+
+  Here are some visualized samples:
+
+  ![result](./result.png)
+
+  Before attack, the accuracy of the target cnn network is **0.9902**, and it becomes **0.2773** after attack.
+
+  The result is not good enough, so **WELCOME CONTRIBUTION !!!**
---
+++ atn.py
@@ -0,0 +1,151 @@
+import tensorflow as tf
+import numpy as np
+import atn_model as atn
+import os
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib  
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_boolean('train', False, 'Train and save the ATN model.')
+
+# Placeholder nodes.
+images_holder = tf.placeholder(tf.float32, [None, 784])
+label_holder = tf.placeholder(tf.float32, [None, 10])
+p_keep_holder = tf.placeholder(tf.float32)
+rerank_holder = tf.placeholder(tf.float32, [None, 10])
+
+mnist = input_data.read_data_sets("./MNIST_data", one_hot=True)
+
+
+def main(arvg=None):
+    """
+    """
+    if FLAGS.train:
+        train()
+    else:
+        test()
+
+
+def test():
+    """
+    """
+    print "ok\n"
+    batch_size = 1000
+    batch_xs, batch_ys = mnist.test.next_batch(batch_size)
+    model = atn.ATN(images_holder, label_holder, p_keep_holder, rerank_holder)
+	
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        model.load(sess, './Models/AE_for_ATN')
+		
+		
+		
+        adv_images = sess.run(
+            model.prediction,
+            #feed_dict={images_holder: mnist.test.images}
+            feed_dict={images_holder: batch_xs}
+        )
+        
+        print('Original accuracy: {0:0.5f}'.format(
+            sess.run(model._target.accuracy, feed_dict={
+                #images_holder: mnist.test.images,
+                #label_holder: mnist.test.labels,
+                images_holder: batch_xs,
+                label_holder: batch_ys,
+                p_keep_holder: 1.0
+            })))
+
+        print('Attacked accuracy: {0:0.5f}'.format(
+            sess.run(model._target.accuracy, feed_dict={
+                images_holder: adv_images,
+                #label_holder: mnist.test.labels,
+                label_holder: batch_ys,
+                p_keep_holder: 1.0
+            })))
+            
+        confidence_origin = sess.run(model._target.showY, feed_dict={
+        						images_holder: batch_xs,
+                				#label_holder: mnist.test.labels,
+                				label_holder: batch_ys,
+                				p_keep_holder: 1.0
+            				})
+        					
+        confidence_adv = sess.run(model._target.showY, feed_dict={
+        						images_holder: adv_images,
+                				#label_holder: mnist.test.labels,
+                				label_holder: batch_ys,
+                				p_keep_holder: 1.0
+            				})
+
+        # Show some results.
+        plt.axis('off')
+        f, a = plt.subplots(2, 10, figsize=(64, 2))
+        
+        for i in range(10):
+            #a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+            a[0][i].axis('off')
+            a[1][i].axis('off')
+            a[0][i].imshow(np.reshape(batch_xs[i], (28, 28)), cmap='Greys_r')
+            a[1][i].imshow(np.reshape(adv_images[i], (28, 28)), cmap='Greys_r')
+        plt.show()
+        plt.savefig('./Result/image.jpg')  
+        
+        #data_file = open("result_data", "w+")
+        #for i in range(0, 10):
+        print(confidence_origin[0])
+        #for i in range(0, 10):
+        print(confidence_adv[0])
+        #data_file.close()
+
+
+def train():
+    """
+    """
+    attack_target = 8
+    alpha = 1.5
+    training_epochs = 10
+    batch_size = 64
+
+    model = atn.ATN(images_holder, label_holder, p_keep_holder, rerank_holder)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        model._target.load(sess, './Models/AE_for_ATN/BasicCNN')
+
+        total_batch = int(mnist.train.num_examples/batch_size)
+        #for epoch in range(training_epochs):
+        for epoch in range(10):
+            for i in range(total_batch):
+            #for i in range(3):
+            	print epoch, training_epochs, i, total_batch 
+                batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+
+                r_res = sess.run(model._target.prediction,
+                                 feed_dict={
+                                     images_holder: batch_xs,
+                                     p_keep_holder: 1.0
+                                 })
+                r_res[:, attack_target] = np.max(r_res, axis=1) * alpha
+                norm_div = np.linalg.norm(r_res, axis=1)
+                for i in range(len(r_res)):
+                    r_res[i] /= norm_div[i]
+
+                _, loss = sess.run(model.optimization, feed_dict={
+                    images_holder: batch_xs,
+                    p_keep_holder: 1.0,
+                    rerank_holder: r_res
+                })
+
+            print('Eopch {0} completed. loss = {1}'.format(epoch+1, loss))
+        print("Optimization Finished!")
+
+        model.save(sess, './Models/AE_for_ATN')
+        print("Trained params have been saved to './Models/AE_for_ATN'")
+
+
+if __name__ == '__main__':
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    tf.app.run()
